4e4ef1bf36d7d9d25a5160582e72784832ebb014
cluice <cluc.dev@gmail.com>
2019-01-03 01:25:17 -0700

    added a class?

--- checkout-test.js
+++ checkout-test.js
@@ -10,6 +10,7 @@ var x = setInterval(function () {
     if (document.getElementById("checkout-shipping-options")) {
         if (c < 1) {
             var div = document.createElement("div");
+            div.setAttribute('class', 'kedge block aftershipping');
             div.innerHTMl = html;
             document.getElementById("checkout-shipping-options").appendChild(div);
             c = c + 1;
