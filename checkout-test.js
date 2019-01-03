bd6c9652e01c5480f7b21dfcaa9c743e730a5dbc
cluice <cluc.dev@gmail.com>
2019-01-03 03:35:55 -0700

    take out moree........................

--- checkout-test.js
+++ checkout-test.js
@@ -4,7 +4,6 @@ var x = setInterval(function () {
         if (c < 1) {
             var div = document.createElement("div");
             div.setAttribute('class', 'kedge block aftershipping');
-            div.innerHTMl = html;
             document.getElementById("checkout-shipping-options").appendChild(div);
             c = c + 1;
         }
